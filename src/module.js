44a49805c1cadadc75d3e6fd00c5b1b4c119a978
regenrek <hello@regenrek.at>
2021-09-25 21:50:27 +0200

    remove c output

--- src/module.js
+++ src/module.js
@@ -14,7 +14,6 @@ export default async function blokModule({
   await requireModule(["@nujek/dynamic", { withConsole, debug }]);
 
   nuxt.hook("components:dirs", (dirs) => {
-    console.log("go for it")
     dirs.push({
       path: resolve(runtimeDir, "components"),
       prefix: '',
